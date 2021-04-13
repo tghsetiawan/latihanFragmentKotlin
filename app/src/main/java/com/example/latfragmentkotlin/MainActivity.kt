8c903f8e71f380008b100d29166d61bd7454465c
tghsetiawan <55044847+tghsetiawan@users.noreply.github.com>
2021-04-13 09:49:55 +0700

    membuat bottom navigation

--- app/src/main/java/com/example/latfragmentkotlin/MainActivity.kt
+++ app/src/main/java/com/example/latfragmentkotlin/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.latfragmentkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.latfragmentkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         val navControl = this.findNavController(R.id.nav_host_fragment_container)
         NavigationUI.setupActionBarWithNavController(this,navControl)
+        
+        NavigationUI.setupWithNavController(binding.bottomNav, navControl)
     }
 
     override fun onSupportNavigateUp(): Boolean {
